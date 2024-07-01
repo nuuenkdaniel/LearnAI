7024c35c1393bdbfb1b1d79d7e647db8b418cc4c
Daniel <lewdaniel2@gmail.com>
2024-07-01 13:01:21 -0400

    began creating single neuron

---
+++ MatrixFunc.java
@@ -0,0 +1,7 @@
+public class MatrixFunc {
+  public static float[] multiply(float[] m1, float[] m2) {}
+
+  public static float[] transpose(float[] m1) {}
+
+  public static float[] add(float[] m1, float[] m2) {}
+}
---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+
+  public static void main(String[] args) {
+    float[] inputs = new float[] {1.1f, 1.2f, 1.3f};
+    float[] weights = new float[] {3.1f, 2.1f, 1.1f};
+    float bias = 3f;
+    
+    float output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias;
+    System.out.println(output);
+  }
+}
