568a336ee8415249ebf10c6247b679c8aec86dd2
Daniel <lewdaniel2@gmail.com>
2024-07-17 13:50:11 -0400

    test function made

--- NeuralNetworkJava/src/Main.java
+++ NeuralNetworkJava/src/Main.java
@@ -1,41 +1,60 @@
 public class Main {
 
+  public static void printLayers(Layer[] layers) {
+    for(int i = 0; i < layers.length; i++) {
+      layers[i].printLayer();
+    }
+  }
+
+  public static void printVector(float[] v) {
+    System.out.print("[");
+    for(int i = 0; i < v.length-1; i++) {
+      System.out.print(v[i]+", ");
+    }
+    System.out.println(v[v.length-1]+"]");
+  }
+
+  public static void run(Layer[] layers, float[] input, int[] catLabels) {
+    System.out.println("Input:");
+    printVector(input);
+    System.out.println();
+
+    for(int i = 0; i < layers.length-1; i++) {
+      System.out.println("Layer "+i+":");
+      input = layers[i].pass(input);
+      System.out.print("  Before ReLU:\n  ");
+      printVector(input);
+      input = Layer.activFunc_ReLU(input);
+      System.out.print("  ReLU:\n  ");
+      printVector(input);
+    }
+    int index = layers.length-1;
+    System.out.println("Layer "+index+": ");
+    input = layers[layers.length-1].pass(input);
+    System.out.print("  Before SoftMax:\n  ");
+    printVector(input);
+    input = Layer.activFunc_softMax(input);
+    System.out.print("  SoftMax:\n  ");
+    printVector(input);
+
+    System.out.println();
+    float loss[] = Layer.catCrossEntropy(input, catLabels);
+    System.out.println("Loss: ");
+    printVector(loss);
+  }
+
   public static void main(String[] args) {
-    //float[] v1 = new float[] {2f, 3f};
-    //float[][] m1 = {
-      //{4f, 2f},
-      //{3f, 1f},
-    //};
-/*
-    float[][] m3 = {
-      {4f, 2f, 5f},
-      {3f, 1f, 6f},
-    };
-
-    float[][] m2 = MatrixFunc.multiply(m1, m3);
-    for(int i = 0; i < m2.length; i++) {
-      for(int j = 0; j < m2[0].length; j++) {
-        System.out.print(m2[i][j]+" ");
-      }
-      System.out.println();
-    }*/
+    
     float[] input = new float[] {200f, 2f};
+    float[][] input2 = new float[0][0];
 
-    Layer inp_layer = new Layer(2, 3);
-    Layer layer1 = new Layer(3, 3);
-    Layer layer2 = new Layer(3, 3);
-    Layer layer3 = new Layer(3, 3);
-    Layer out_layer = new Layer(3, 2);
+    Layer[] layers = new Layer[5];
+    layers[0] = new Layer(2, 3);
+    layers[1] = new Layer(3, 3);
+    layers[2] = new Layer(3, 3);
+    layers[3] = new Layer(3, 3);
+    layers[4] = new Layer(3, 2);
 
-    inp_layer.printLayer();
-    layer1.printLayer();
-    layer2.printLayer();
-    layer3.printLayer();
-    out_layer.printLayer();
-
-    float[] output = out_layer.pass(Layer.activFunc_ReLU(layer3.pass(Layer.activFunc_ReLU(layer2.pass(Layer.activFunc_ReLU(inp_layer.pass(input)))))));
-    for(int i = 0; i < output.length; i++) System.out.println(output[i]);
-    output = Layer.activFunc_softMax(output);
-    for(int i = 0; i < output.length; i++) System.out.println(output[i]);
+    run(layers, input, new int[] {0});
   }
 }
